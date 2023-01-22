2ae79a45ceed2870a56e658dd412a1c9efa75ee8
Rizch Jed Perez <fleetfeat0989@gmail.com>
2023-01-22 14:25:38 +0800

    prettier

--- src/app/records/record-list/record-list.component.ts
+++ src/app/records/record-list/record-list.component.ts
@@ -1,26 +1,31 @@
 import { RecordsComponent } from './../records.component';
 import { DataServiceService } from './../../data-service.service';
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: "records-record-list",
-  templateUrl: "./record-list.component.html",
-  styleUrls: ["./record-list.component.css"],
+  selector: 'records-record-list',
+  templateUrl: './record-list.component.html',
+  styleUrls: ['./record-list.component.css'],
 })
 export class RecordListComponent {
   @Input() tableData: { name: string; email: string; mobileNumber: number }[] =
     [];
 
-  constructor(private dataService: DataServiceService,
-              private deleteService: RecordsComponent) {}
+  constructor(
+    private dataService: DataServiceService,
+    private deleteService: RecordsComponent
+  ) {}
 
-
-  onDisplay(displayData: { name: string; email: string; mobileNumber: number }){
-    this.dataService.dataViewing.splice(0,1);
+  onDisplay(displayData: {
+    name: string;
+    email: string;
+    mobileNumber: number;
+  }) {
+    this.dataService.dataViewing.splice(0, 1);
     this.dataService.dataViewing.push(displayData);
   }
 
-  onDelete(id: string){
-   this.deleteService.onDeleteData(id);
+  onDelete(id: string) {
+    this.deleteService.onDeleteData(id);
   }
 }
